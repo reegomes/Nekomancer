c6325a5e583db3780fcc93de4d5b8ef71981cf92
Renato Gomes <reegomescel@gmail.com>
2018-04-27 22:20:41 -0300

    Commit

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class CameraScript : MonoBehaviour {
 
- public float dampTime = 0.5f;  // Valor da suavização
+ public float dampTime = 2.0f;  // Valor da suavização
  private Vector3 velocity = Vector3.zero;  // Velocidade da camera
  public Transform target;  // Momochi
  void Update () 
@@ -12,8 +12,9 @@ public class CameraScript : MonoBehaviour {
 		 if (target)
 		 {
 			Vector3 point = GetComponent<Camera> ().WorldToViewportPoint (target.position);
-			Vector3 delta = target.position - GetComponent<Camera> ().ViewportToWorldPoint (new Vector3 (0.5f, 0.5f, point.z));
-   			Vector3 destination = transform.position + delta;
+            //Vector3 delta = target.position - GetComponent<Camera> ().ViewportToWorldPoint (new Vector3 (0.5f, 0.5f, point.z));
+            Vector3 delta = target.position - GetComponent<Camera>().ViewportToWorldPoint(new Vector3(0.1f, 0.2f, point.z));
+            Vector3 destination = transform.position + delta;
    			transform.position = Vector3.SmoothDamp (transform.position, destination, ref velocity, dampTime);
   		}
  	}
