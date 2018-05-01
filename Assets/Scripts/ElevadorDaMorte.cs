a04a9f4e2decafd27f50af0193c7b84cb968543b
Renato Gomes <reegomescel@gmail.com>
2018-05-01 20:05:40 -0300

    commit dell

--- Assets/Scripts/ElevadorDaMorte.cs
+++ Assets/Scripts/ElevadorDaMorte.cs
@@ -4,20 +4,49 @@ using UnityEngine;
 
 public class ElevadorDaMorte : MonoBehaviour {
 
-	float subindo = 10f;
+	public bool sobe, desce;
 	// Use this for initialization
 	void Start () {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	public void Update () {
+		if (sobe == true){
+			transform.Translate(new Vector3(0, 5.0f * Time.deltaTime, 0));
+			desce = false;
+		}
+		else if (desce == true)
+		{
+			transform.Translate(new Vector3(0, -5.0f * Time.deltaTime, 0));
+			sobe = false;
+		}
+		else
+		{
+		desce = false;
+		sobe = false;
+		}
 	}
-
-	void OnTriggerExit2D(Collider2D other) {
-		if (this.gameObject.CompareTag("caixas")){
-			
-		}	
+	public void OnCollisionExit2D(Collision2D caixas)
+	{
+		if (caixas.gameObject.CompareTag("caixas"))
+		{
+			Debug.Log("Saida da caixa ok");
+			sobe = true;
+		}
+	}
+	public void OnCollisionEnter2D(Collision2D other)
+	{
+		if (other.gameObject.CompareTag("paraescada"))
+		{
+			Debug.Log("Colidiu com o para colisao");
+			sobe = false;
+			desce = true;
+		}
+		if (other.gameObject.CompareTag("sobearmadilha"))
+		{
+			desce = false;
+			sobe = true;
+		}
 	}
 }
